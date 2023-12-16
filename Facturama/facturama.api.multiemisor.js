a4f84ebdc2d967074735ff74c11a0b767cffebfe
Moshe Shabes <moshnaf@gmail.com>
2023-12-16 12:16:08 -0600

    Add new files and update documentation

--- Facturama/facturama.api.multiemisor.js
+++ Facturama/facturama.api.multiemisor.js
@@ -1,191 +1,94 @@
+const axios = require('axios').default;
 /*
 Soporte API Facturama
-    soporte-api@facturama.mx
+	soporte-api@facturama.mx
 */
 
-var valuesFacturama = {
-    token: "cHJ1ZWJhczpwcnVlYmFzMjAxMQ==", // usuario: pruebas, Contraseña: pruebas2011
-    url: "https://apisandbox.facturama.mx/"
-};
 
-$.ajaxSetup({
-    beforeSend: function (xhr) {
-        xhr.withCredentials = true;
-        xhr.setRequestHeader("Authorization", "Basic " + valuesFacturama.token);
-    }
+
+const valuesFacturama = {
+	token: "TW9zaGVOYWZ0YWxpOlhaYW9EeFM3cjImV295", // usuario: pruebas, Contraseña: pruebas2011
+	url: "https://apisandbox.facturama.mx/"
+};
+const instance = axios.create({
+	baseURL: valuesFacturama.url,
 });
 
-(function (window) {
-    function facturama() {
+instance.defaults.headers.common["Authorization"] = "Basic " + valuesFacturama.token;
 
-        var settings = {
-            url: valuesFacturama.url
-        };
+const facturama = () => {
 
-        function retrieve(path, id, callback) {
-            return jQuery.get(settings.url + path + '/' + id, undefined, callback, "json");
-        }
+	const retrieve = (path, id) => {
+		return instance.get(path + '/' + id).then(response => response.data);
+	};
 
-        function list(path, callback) {
-            return jQuery.get(settings.url + path, undefined, callback, "json");
-        }
+	const list = (path) => {
+		return instance.get(path).then(response => response.data);
+	};
 
-        function listWithParam(path, param, callback) {
-            return jQuery.get(settings.url + path + param, undefined, callback, "json");
-        }
+	const listWithParam = (path, param) => {
+		return instance.get(path + param).then(response => response.data);
+	};
 
-        function postSyncWithParam(path, param, callback) {
-            $.ajax({
-                url: settings.url + path + param,
-                type: 'POST',
-                async: false,
-                success: callback
-            });
-        }
+	const postSyncWithParam = (path, param) => {
+		return instance.post(path + param).then(response => response.data);
+	};
 
-        function postSyncWithData(path, data, callback, callbackError) {
-            $.ajax({
-                url: settings.url + path,
-                type: 'POST',
-                async: false,
-                dataType: 'json',
-                data: JSON.stringify(data),
-                contentType: "application/json",
-                success: callback,
-                error: callbackError
-            });
-        }
+	const postSyncWithData = (path, data) => {
+		return instance.post(path, data, {
+			headers: {
+				'Content-Type': 'application/json',
+			}
+		}).then(response => response.data);
+	};
 
-        function putSyncWithData(path, data, callback, callbackError) {
-            $.ajax({
-                url: settings.url + path,
-                type: 'PUT',
-                async: false,
-                data: JSON.stringify(data),
-                contentType: "application/json",
-                success: callback,
-                error: callbackError
-            });
-        }
+	const putSyncWithData = (path, data) => {
+		return instance.put(path, data, {
+			headers: {
+				'Content-Type': 'application/json',
+			}
+		}).then(response => response.data);
+	};
 
-        function deleteSyncWithParam(path, param, callback, callbackError) {
-            $.ajax({
-                url: settings.url + path + '/' + param,
-                type: 'DELETE',
-                async: false,
-                success: callback,
-                error: callbackError
-            });
-        }
+	const deleteSyncWithParam = (path, param) => {
+		return instance.delete(path + '/' + param).then(response => response.data);
+	};
 
-        var facturamaObject = {
-            Cfdi: {
-                Get: function (id, callback) {
-                    return retrieve('api-lite/cfdis', id, callback);
-                },
-                List: function (param, callback) {
-                    return listWithParam('api-lite/cfdis', param, callback);
-                },
-                Create: function (data, callback, callbackError) {
-                    postSyncWithData('api-lite/2/cfdis', data, callback, callbackError);
-                },
-                
-                Create3: function (data, callback, callbackError) {
-                    postSyncWithData('api-lite/3/cfdis', data, callback, callbackError);
-                },
-                Send: function (param, callback) {
-                    postSyncWithParam('cfdi', param, callback);
-                },
-                Cancel: function (params, callback, callbackError) {
-                    deleteSyncWithParam('api-lite/cfdis', params, callback, callbackError);
-                },
-                Download: function (format, type, id, callback) {
-                    retrieve('cfdi/' + format + '/' + type, id, callback);
-                },
-                Acuse: function (format, type, id, callback) {
-                    retrieve('acuse/' + format + '/' + type, id, callback);
-                }
-            },
-            Certificates: {
-                Get: function (param, callback) {
-                    return listWithParam('api-lite/csds/', param, callback);
-                },
-                List: function (callback) {
-                    return list('api-lite/csds', callback);
-                },
-                Create: function (data, callback, callbackError) {
-                    postSyncWithData('api-lite/csds', data, callback, callbackError);
-                },
-                Update: function (param, data, callback, callbackError) {
-                    putSyncWithData('api-lite/csds/' + param, data, callback, callbackError);
-                },
-                Remove: function (params, callback, callbackError) {
-                    deleteSyncWithParam('api-lite/csds', params, callback, callbackError);
-                }
-            },
-            Catalogs: {
-                States: function (country, callback) {
-                    return list('catalogs/municipalities?countryCode' + country, callback);
-                },
-                Municipalities: function (state, callback) {
-                    return list('catalogs/municipalities?stateCode' + state, callback);
-                },
-                Localities: function (state, callback) {
-                    return list('catalogs/localities?stateCode' + state, callback);
-                },
-                Neighborhoods: function (postalCode, callback) {
-                    return list('catalogs/neighborhoods?postalCode' + postalCode, callback);
-                },
-                ProductsOrServices: function (keyword, callback) {
-                    return list('catalogs/ProductsOrServices?keyword=' + keyword, callback);
-                },
-                PostalCodes: function (keyword, callback) {
-                    return list('catalogs/PostalCodes?keyword=' + keyword, callback);
-                },
-                Units: function (callback) {
-                    return list('catalogs/Units', callback);
-                },
-                Currencies: function (callback) {
-                    return list('catalogs/Currencies', callback);
-                },
-                Countries: function (callback) {
-                    return list('catalogs/Countries', callback);
-                },
-                PaymentForms: function (callback) {
-                    return list('catalogs/PaymentForms', callback);
-                },
-                PaymentMethods: function (callback) {
-                    return list('catalogs/PaymentMethods', callback);
-                },
-                FederalTaxes: function (callback) {
-                    return list('catalogs/FederalTaxes', callback);
-                },
-                FiscalRegimens: function (callback) {
-                    return list('catalogs/FiscalRegimens', callback);
-                },
-                CfdiTypes: function (callback) {
-                    return list('catalogs/CfdiTypes', callback);
-                },
-                RelationTypes: function (callback) {
-                    return list('catalogs/RelationTypes', callback);
-                },
-                CfdiUses: function (keyword, callback) {
-                    return list('catalogs/CfdiUses?keyword=' + keyword, callback);
-                }
-            }
-        };
+	const facturamaObject = {
+		Cfdi: {
+			Get: (id) => retrieve('api-lite/cfdis', id),
+			List: (param) => listWithParam('api-lite/cfdis', param),
+			Create: (data) => postSyncWithData('api-lite/2/cfdis', data),
+			Create3: (data) => postSyncWithData('api-lite/3/cfdis', data),
+			Send: (param) => postSyncWithParam('cfdi', param),
+			Cancel: (params) => deleteSyncWithParam('api-lite/cfdis', params),
+			Download: (format, type, id) => retrieve('cfdi/' + format + '/' + type, id),
+			Acuse: (format, type, id) => retrieve('acuse/' + format + '/' + type, id)
+		},
+		Certificates: {
+			Get: (param) => listWithParam('api-lite/csds/', param),
+			List: () => list('api-lite/csds'),
+			Create: (data) => postSyncWithData('api-lite/csds', data),
+			Update: (param, data) => putSyncWithData('api-lite/csds/' + param, data),
+			Remove: (params) => deleteSyncWithParam('api-lite/csds', params)
+		},
+		Catalogs: {
+			States: (country) => list('catalogs/municipalities?countryCode=' + country),
+			Municipalities: (state) => list('catalogs/municipalities?stateCode=' + state),
+			Localities: (state) => list('catalogs/localities?stateCode=' + state),
+			Neighborhoods: (postalCode) => list('catalogs/neighborhoods?postalCode=' + postalCode),
+			ProductsOrServices: (keyword) => list('catalogs/ProductsOrServices?keyword=' + keyword),
+			Units: (keyword) => list('catalogs/Units?keyword=' + keyword),
+			CFDIUses: () => list('catalogs/CfdiUses'),
+			FiscalRegimens: () => list('catalogs/FiscalRegimens'),
+			PaymentForms: () => list('catalogs/PaymentForms'),
+			PaymentMethods: () => list('catalogs/PaymentMethods'),
+			NameIds: () => list('catalogs/NameIds'),
+		},
+		getToken: () => "Basic " + valuesFacturama.token,
+	};
 
-        facturamaObject.getToken = function () {
-            return "Basic " + valuesFacturama.token;
-        };
+	return facturamaObject;
+};
 
-        facturamaObject.getBaseUri = function () {
-            return settings.url;
-        };
-
-        return facturamaObject;
-    }
-    if (window.Facturama === undefined) {
-        window.Facturama = facturama();
-    }
-})(window);
+module.exports = facturama;
